c74a4b5715eecee4bbcbce05e7f22e74596c33f1
hooftly <mhooft@tf-dev.org>
2018-11-06 13:03:04 -0700

    ?

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-var api = 'http://184.68.225.218:31018';
+var api = 'https://184.68.225.218:31018';
 var donationAddress = "";
 var blockTargetInterval = 120;
 var coinUnits = 10000;
@@ -7,7 +7,7 @@ var refreshDelay = 30000;
 // pools stats by MainCoins
 var networkStat = {
  "amit": [
-	["184.68.225.218:31018", "http://184.68.225.218:31018"]
+	["184.68.225.218:31018", "https://184.68.225.218:31018"]
 ]
 };
 
