93bab63cc78e622b6ea4fdc65a81ff8a80b0c1eb
hooftly <mhooft@tf-dev.org>
2018-11-06 18:14:30 -0700

    fix plz

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-var api = 'http://184.68.225.218/api/';
+var api = 'http://explorer.getamitycoin.org/api/';
 var donationAddress = "";
 var blockTargetInterval = 120;
 var coinUnits = 10000;
@@ -7,7 +7,7 @@ var refreshDelay = 30000;
 // pools stats by MainCoins
 var networkStat = {
  "amit": [
-	["184.68.225.218", "http://184.68.225.218/api/"]
+	["explorer.getamitycoin.org", "http://getamitycoin.org/api/"]
 ]
 };
 
