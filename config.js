1525751eec76b0e5f4111cd3e6b896a35acb7e85
hooftly <mhooft@tf-dev.org>
2018-11-08 13:20:11 -0700

    Digit Fix

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var api = 'https://explorer.getamitycoin.org/api';
 var donationAddress = "";
 var blockTargetInterval = 120;
-var coinUnits = 100000;
+var coinUnits = 1000000;
 var symbol = 'AMIT';
 var refreshDelay = 30000;
 // pools stats by MainCoins
